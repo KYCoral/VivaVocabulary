9db12a8963a18a826d142395df945a6cd03cc6be
Yoro <izumiyoro@iskolarngbayan.pup.edu.ph>
2024-06-13 01:15:18 +0800

    level2
    -the level2 scene also changed its screen resolution,
    -player can now see the words clearly.
    increase difficulty was toned-down

--- levelMedium.gd
+++ levelMedium.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 @onready var Enemy: PackedScene = preload("res://Enemy.tscn")
 
-@onready var enemy_container = $EnemyContainer
-@onready var spawn_container = $SpawnContainer
-@onready var spawn_timer = $SpawnTimer
-@onready var difficulty_timer = $DifficultyTimer
+@onready var buttonStart: Button = $CanvasLayer2/startGame/CenterContainer/VBoxContainer/start
+@onready var enemy_container = $CanvasLayer/VBoxContainer/MiddleRow/EnemyContainer
+@onready var spawn_container = $CanvasLayer/VBoxContainer/MiddleRow/SpawnContainer
+@onready var spawn_timer: Timer = $SpawnTimer
+@onready var difficulty_timer: Timer = $DifficultyTimer
 
 @onready var difficulty_value = $CanvasLayer/VBoxContainer/BottomRow/HBoxContainer/DifficultyValue
 @onready var killed_value = $CanvasLayer/VBoxContainer/TopRow2/TopRow/EnemiesKilledValue
 @onready var game_over_screen = $CanvasLayer/GameOverScreen
-
+@onready var start_screen = $CanvasLayer2/startGame
 
 @onready var gameOver_value = $CanvasLayer/GameOverScreen/CenterContainer/VBoxContainer/WordCount
 
@@ -20,14 +21,21 @@ var current_letter_index: int = -1
 var difficulty: int = 0
 var enemies_killed: int = 0
 
-
-
-func _ready():
+func _ready() -> void:
 	# Connect the timers to their respective functions
-	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
-	difficulty_timer.timeout.connect(_on_difficulty_timer_timeout)
+	spawn_timer.timeout.connect(self._on_spawn_timer_timeout)
+	difficulty_timer.timeout.connect(self._on_difficulty_timer_timeout)
+	buttonStart.button_down.connect(self._on_start_button_down)
+	
+	spawn_timer.stop()
+	difficulty_timer.stop()
+	active_enemy = null
+	current_letter_index = -1
+	gameOver_value.text = str(enemies_killed)
+	for enemy in enemy_container.get_children():
+		enemy.queue_free()
 
-func find_new_active_enemy(typed_character: String):
+func find_new_active_enemy(typed_character: String) -> void:
 	for enemy in enemy_container.get_children():
 		var prompt = enemy.get_prompt()
 		var next_character = prompt.substr(0, 1)
@@ -39,10 +47,9 @@ func find_new_active_enemy(typed_character: String):
 			return
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+	if event is InputEventKey and event.pressed and not event.echo:
 		var typed_event = event as InputEventKey
 		var key_typed = PackedByteArray([typed_event.unicode]).get_string_from_utf8()
-
 		if active_enemy == null:
 			find_new_active_enemy(key_typed)
 		else:
@@ -65,7 +72,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_spawn_timer_timeout() -> void:
 	spawn_enemy()
 
-func spawn_enemy():
+func spawn_enemy() -> void:
 	var enemy_instance = Enemy.instantiate()
 	var spawns = spawn_container.get_children()
 	var index = randi() % spawns.size()
@@ -73,12 +80,12 @@ func spawn_enemy():
 	enemy_container.add_child(enemy_instance)
 	enemy_instance.set_difficulty(difficulty)
 
-func _on_difficulty_timer_timeout():
+func _on_difficulty_timer_timeout() -> void:
 	difficulty += 1
 	GlobalSignals.emit_signal("difficulty_increased", difficulty)
 	print("Difficulty increased to %d" % difficulty)
 	var new_wait_time = spawn_timer.wait_time - 0.2
-	spawn_timer.wait_time = clamp(new_wait_time, 1, spawn_timer.wait_time)
+	spawn_timer.wait_time = clamp(new_wait_time, 1.0, spawn_timer.wait_time)
 	difficulty_value.text = str(difficulty)
 	
 	if difficulty >= 20:
@@ -89,7 +96,7 @@ func _on_difficulty_timer_timeout():
 func _on_lose_area_body_entered(_body: Node) -> void:
 	game_over()
 
-func game_over():
+func game_over() -> void:
 	game_over_screen.show()
 	spawn_timer.stop()
 	difficulty_timer.stop()
@@ -99,7 +106,7 @@ func game_over():
 	for enemy in enemy_container.get_children():
 		enemy.queue_free()
 
-func start_game():
+func start_game() -> void:
 	game_over_screen.hide()
 	difficulty = 0
 	enemies_killed = 0
@@ -110,5 +117,19 @@ func start_game():
 	difficulty_timer.start()
 	spawn_enemy()
 
-func _on_restart_button_pressed():
+func _on_restart_button_pressed() -> void:
 	start_game()
+
+func _on_start_button_down() -> void:
+	start_screen.hide()
+	difficulty = 0
+	enemies_killed = 0
+	difficulty_value.text = str(0)
+	killed_value.text = str(0)
+	randomize()
+	spawn_timer.start()
+	difficulty_timer.start()
+	spawn_enemy()
+
+func _on_map_button_down() -> void:
+	pass # Replace with function body.
